58a82402b94680caa4aa41d14e9e0051367da08b
“DavidAmyot” <davidamyot@me.com>
2020-05-01 01:14:07 -0400

    Finally working post request for speech to text

--- app/controllers/recognition_controller.rb
+++ app/controllers/recognition_controller.rb
@@ -1,199 +1,47 @@
 require 'net/http'
 require 'json'
-require 'find'
-
 
 class RecognitionController < ApplicationController
-  protect_from_forgery
-#   before_action :authenticate_user!
-#  before_action :authorize_admins_and_employees, only: :index
+    protect_from_forgery
+    #  before_action :authenticate_user!
+    #  before_action :authorize_admins_and_employees, only: :index
 
-  skip_before_action :verify_authenticity_token
+    skip_before_action :verify_authenticity_token
 
-
-  def authorize_admins_and_employees
-    redirect_to index_path, status: 401 unless current_user.role.id <= 2
-    # puts "Current user role is #{current_user.role.id}"
-  end
-
-  def index
-
-
-  end
-
-  def upload
-      puts params.inspect
-      puts params[:speech]
-      file_name = sanitize_filename(params[:attachement1])
-      puts "File name : "+file_name
-      puts "Key: "+ENV["AZURESPEAKERKEY"]
-      binaryOfFile = createBinary(file_name)
-      if(!binaryOfFile.nil?)
-          p "Create the indentification profile ..."
-          
-            identificationProfileIds = createProfile()
-            puts identificationProfileIds
-            hash = JSON.parse(identificationProfileIds)
-            puts hash["identificationProfileId"]
-            
-            idProfil =  hash["identificationProfileId"]
-            # idProfil = '92489933-e1e7-4fea-97d2-2084d427ad9d'
-            p "Create profile  enrolement..."
-          
-          http_url = 'https://speechelevators.cognitiveservices.azure.com/spid/v1.0/identificationProfiles/'+idProfil.to_s+'/enroll'
-          puts "create enrollement: "+http_url
-          uri = URI(http_url)
-          uri.query = URI.encode_www_form({
-              # Request parameters
-              'shortAudio' => 'true'
-          })
-
-          request = Net::HTTP::Post.new(uri.request_uri)
-          # Request headers
-          request['Content-Type'] = 'application/octet-stream'
-          # Request headers
-          request['Ocp-Apim-Subscription-Key'] = ENV["AZURESPEAKERKEY"]
-          # Request body
-          request.body = binaryOfFile
-
-          response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
-              http.request(request)
-          end
-          
-          puts response.body
-
-      end
-
-  end
-
-  def sanitize_filename(file_name)
-      just_filename = File.basename(file_name)
-      puts just_filename 
-      just_filename.sub(/[^\w\.\-]/,'_')
-  end
-
-  def createProfile
-        p "Create a profil ..."
-        p "Key:"+ENV["AZURESPEAKERKEY"]
-      uri = URI('https://speechelevators.cognitiveservices.azure.com/spid/v1.0/identificationProfiles')
-      uri.query = URI.encode_www_form({
-      })
-
-      request = Net::HTTP::Post.new(uri.request_uri)
-      # Request headers
-      request['Content-Type'] = 'application/json'
-      # Request headers
-      request['Ocp-Apim-Subscription-Key'] = ENV["AZURESPEAKERKEY"]
-      # Request body
-      request.body = "{ \"locale\":\"en-us\"}"
-
-      response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
-          http.request(request)
-      end
-
-      response.body
-  end
-
-  def createBinary(path="")
-
-      p Rails.root
-      absPath = File.expand_path('public/audios/'+path)
-      puts "dddd "
-      puts absPath
-      if(File.exists?(absPath))
-          puts "woooooooo "
-          return File.binread(absPath)
-      end
-      
-  end
-
-
-  def audiorecognition
-    puts "audio recognition "
-    puts params.inspect
-    filename = params[:filename]
-    absPath = File.expand_path('public/audios/'+filename)
-    puts absPath
-    if(File.exists?(absPath))
-        puts "woooooooo "
-        azurerecognitionapi(File.binread(absPath))
+    def authorize_admins_and_employees
+        redirect_to index_path, status: 401 unless current_user.role.id <= 2
+        # puts "Current user role is #{current_user.role.id}"
     end
-  end
 
-  def azurerecognitionapi(binary)
-    hash = getAllIndentificationProfil
-    ids = getIdProfils(hash)
-    if !ids.nil? 
-        voiceidentification(ids, binary)
-    else 
-        p "No profil ids found ...."
+    def index
     end
+
+    def upload
+        # Using the read method to get the uploaded file as binary
+        file = params[:uploadfile].read
     
-  end
+        uri = URI('https://eastus.stt.speech.microsoft.com/speech/recognition/conversation/cognitiveservices/v1?cid=010c08ac-77a7-457b-8957-32d5714a6a91')
+        puts "================================================="
+        puts "URL : #{uri}"
+        puts "================================================="
 
-  def getIdProfils(hash) 
-    ids = []
-    p "retrieve ids"
-    hash.each do |key, value|
-        key.each do |k,v|
-            if(k == "identificationProfileId")
-                ids.push(v)
-            end
-        end
-    end
+        result = Net::HTTP.new(uri.host, uri.port)
 
-    p "your created profil ids"
-    puts ids.take(10).join(",")
-    return ids.take(10).join(",")
-  end
+        # Creating the POST request with the content-type & key header along with the binary file upload as body
+        request = Net::HTTP::Post.new(uri)
+        request['Content-Type'] = 'application/json'
+        request['Ocp-Apim-Subscription-Key'] = 'f8e9d0c119b54890bc4f308440792fef'
+        request.body = file
 
-  def voiceidentification(profilids, binaryfile)
+        # Forcing ssl otherwise it we'll get a broken pipe error since we're using https
+        result.use_ssl = true
 
-    p "State identification ....."
-    #http_url = 'https://speechelevators.cognitiveservices.azure.com/spid/v1.0/identify?identificationProfileIds='+profilids.to_s
-    _http_url='https://speechelevators.cognitiveservices.azure.com/spid/v1.0/identify?identificationProfileIds=d8a4a75a-9d90-4706-a796-9455dc87206a'
-    p "Voice identification endpoint: "+_http_url
-        
-    uri = URI(_http_url)
-    uri.query = URI.encode_www_form({
-    })
+        response = result.request(request)
+        puts "================================================="
+        pp response.body
+        puts "================================================="
 
-    request = Net::HTTP::Post.new(uri.request_uri)
-    # Request headers
-    request['Content-Type'] = 'application/octet-stream'
-    request['Content-Length'] = 0
-    # Request headers
-    request['Ocp-Apim-Subscription-Key'] = ''
-    # Request body
-    request.body = ""
-
-    response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
-        http.request(request)
-    end
-
-    puts response.body
-
-  end
-
-  def getAllIndentificationProfil
-
-    uri = URI('https://speechelevators.cognitiveservices.azure.com/spid/v1.0/identificationProfiles')
-    uri.query = URI.encode_www_form({
-    })
-
-    request = Net::HTTP::Get.new(uri.request_uri)
-    # Request headers
-    request['Ocp-Apim-Subscription-Key'] = ENV["AZURESPEAKERKEY"]
-    # Request body
-    request.body = ""
-
-    response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
-        http.request(request)
-    end
-
-    puts response.body
-
-    return JSON.parse(response.body)
-  end 
-
+        # Rendering the transcription on recognition/upload route
+        render json: response.body
+    end   
 end
