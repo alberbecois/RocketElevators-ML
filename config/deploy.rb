9d7050504d71ad0da555a3a839e749f1000776dd
“DavidAmyot” <davidamyot@me.com>
2020-05-01 22:08:21 -0400

    Modified deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,8 +3,8 @@ lock "~> 3.12.0"
 
 before 'deploy', 'rvm1:install:ruby'
 set :rvm_map_bins, [ 'rake', 'gem', 'bundle', 'ruby', 'puma', 'pumactl' ]
-set :application, "Rocket-Elevator-Foundation"
-set :repo_url, "https://github.com/DavidAmyot/Rocket-Elevator-Foundation"
+set :application, "RocketElevators-ML"
+set :repo_url, "https://github.com/alberbecois/RocketElevators-ML"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
